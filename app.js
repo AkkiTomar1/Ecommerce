679a0ab999e12f6afa757ccf8286ae2a2627add8
Akhilesh.Singh <Akhilesh.Singh2@NW18.COM>
2026-01-29 15:49:55 +0530

    added cors

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ const app = express();
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log('MongoDB Connected');
   createDefaultAdmin(); // Hardcoded admin create
